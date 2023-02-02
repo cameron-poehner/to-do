96925423353628dcc1719146ae3e7145c158097f
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-02 15:35:23 -0800

    Updated Client styles and components folder structures.

---
+++ client/src/styles/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles';
+
+export const StyledContainer = styled('div')(({ theme }) => ({
+    backgroundColor: 'blue',
+    color: 'white',
+    margin: 0,
+    padding: 0,
+}))
