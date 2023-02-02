34ae0c4fb835fd783a782c45c0ee92f09ee58ac4
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-02 15:16:03 -0800

    Updated frontend with initial styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { StyledContainer } from './styles/styles';
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <StyledContainer>
+      <h1>Hello world</h1>
+    </StyledContainer>
   );
 }
 
