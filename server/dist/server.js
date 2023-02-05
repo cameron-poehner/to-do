5157cce3b436e956a7de22322b82414840a69669
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-05 14:56:35 -0800

    Added @types/node to setup local environment variables.

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,6 +1,10 @@
+var _a;
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8000;
+app.get('/', (req, res) => {
+    res.send('Hey Cam!😎');
+});
 app.listen(PORT, () => {
     console.log(`Example running on port ${PORT}`);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ?? 8000;
+
+app.get('/', (req, res) => {
+    console.log('Process', process.env)
+    res.send('Hey Cam! you beautiful bastard!');
+})
 
 app.listen(PORT, () => {
     console.log(`Example running on port ${PORT}`);
