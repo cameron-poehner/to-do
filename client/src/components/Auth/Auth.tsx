394db05c3dc7294e3121bf40792f03a59c288535
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-05 13:46:17 -0800

    Front end styles. Database and backend setup.

---
+++ client/src/components/Auth/Auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledAuthContainer } from './styles'
+
+const Auth = () => {
+    return (
+        <StyledAuthContainer>
+
+        </StyledAuthContainer>
+    )
+}
+
+export default Auth;
---
+++ client/src/components/ListItem/ListItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListItem = () => {
+    return (
+        <div>
+            <h4>Yes</h4>
+        </div>
+    )
+}
+
+export default ListItem;
