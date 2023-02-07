f10633afc89ebaf255312151379c70bd87264664
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-07 15:22:00 -0800

    Added initial get route. Setup db connection and returned todo's.

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT ?? 8000;
+const dbPool = require('./db');
 
-app.get('/', (req, res) => {
-    console.log('Process', process.env)
-    res.send('Hey Cam! you beautiful bastard!');
+app.get('/todos', async (req, res) => {
+    const userEmail = 'cam@test.com';
+
+    try {
+        const todos = await dbPool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
+        res.json(todos.rows);
+    } catch (err) {
+        console.error(err);
+    }
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`Example running on port ${PORT}`);
