23d04d81ef317c2637833d3958b4f4e6cead7040
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-07 16:00:34 -0800

    Setup cors and dynamic todo route to support front end development.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyledPage } from './styles/styles'
 // import Auth from './components/Auth';
 import ListHeader from './components/ListHeader';
@@ -9,12 +9,19 @@ import { theme } from './styles/styles';
 const App = () => {
 
   const getData = async () => {
+    const userEmail = 'cam@test.com';
     try {
-      const res = await fetch(`http://localhost:8000/todos`)
+      const res = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await res.json();
+      console.log('json', json);
     } catch (err) {
       console.error(err)
     }
   }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT ?? 8000;
 const dbPool = require('./db');
 
-app.get('/todos', async (req, res) => {
-    const userEmail = 'cam@test.com';
+app.use(cors());
+
+app.get('/todos/:userEmail', async (req, res) => {
+    const { userEmail } = req.params;
 
     try {
         const todos = await dbPool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
