447139d5b211111bc81b6809c8c5cae929bac921
Cameron Poehner <cameronpoehner@gmail.com>
2023-02-10 16:33:54 -0800

    Setup POST endpoint in server.

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT ?? 8000;
 const dbPool = require('./db');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(cors());
 
@@ -16,9 +17,24 @@ app.get('/todos/:userEmail', async (req, res) => {
     } catch (err) {
         console.error(err);
     }
-})
+});
 
-
+app.post('/todos', async (req, res) => {
+    const id = uuidv4();
+    const {
+        user_email,
+        title,
+        notes,
+        progress,
+        date
+    } = req.body;
+    try {
+        dbPool.query(`Insert INTO todos(id, user_email, title, notes, progress, data) VALUES($1, $2, $3, $4, $5, $6)`,
+            [id, user_email, title, notes, progress, date])
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Example running on port ${PORT}`);
